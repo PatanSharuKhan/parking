f97de0a14840a8798f434acba5c0b267e4264389
patansharukhan <patansharu654321@gmail.com>
2023-07-15 00:06:11 +0530

    modified into classes and methods in module.

--- main.rb
+++ main.rb
@@ -1,27 +1,6 @@
-require "./methods.rb"
+# frozen_string_literal: true
 
-BEGIN {
-    system "clear"
-    puts "*"*30
-    puts "PARKING MANAGEMENT SYSTEM"
-    puts "*"*30
-    puts
-}
+require './methods'
 
-parking=Parking.new
-
-while true
-    askTaskOption
-    option=gets.chomp
-    break if option=="0"
-    activateTask option,parking
-    sleep 1
-end
-
-END{
-    system "clear"
-    puts
-    puts "*"*30
-    puts "THANK YOU ! VISIT AGAIN !"
-    puts "*"*30
-}
+parking = Methods::Parking.new
+parking.run
