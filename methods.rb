fb8ae5c2bd26d38db7cf2b4dbc24cdb63562c5be
patansharukhan <patansharu654321@gmail.com>
2023-07-17 16:54:14 +0530

    modified some code

--- methods.rb
+++ methods.rb
@@ -3,17 +3,21 @@
 module Methods
   # parking class
   class Parking
-    @slots_count = 0
-    @vehicles_count = 0
-    @empty_slots_count = 0
+    $slots_count = 0
+    $vehicles_count = 0
+    $empty_slots_count = 0
 
     def initialize
       @slots = []
     end
 
     def display_task_options
-      puts "Select the following task option: \n 1 - Create Slot \n 2 - Add vehicle \n
-       3 - Remove vehicle \n 4 - View slots \n 0 - Quit the parking area !"
+      puts 'Select the following task option:'
+      puts '1 - Create Slot'
+      puts '2 - Add vehicle'
+      puts '3 - Remove vehicle'
+      puts '4 - View slots'
+      puts "0 - Quit the parking area ! \n \n"
     end
 
     def run
@@ -22,21 +26,21 @@ module Methods
         user_option = gets.chomp
         break if user_option == '0'
 
-        complete_task user_option
+        activate_selected_option user_option
       end
     end
 
     def add_slot
       @slots << 0
-      @slots_count += 1
-      @empty_slots_count += 1
+      $slots_count += 1
+      $empty_slots_count += 1
       puts 'New slot is created!'
     end
 
     def display_parking_details
-      puts "Total slots - #{@slots_count}"
-      puts "Vehicles count - #{@vehicles_count}"
-      puts "Empty slots count - #{@empty_slots_count}"
+      puts "Total slots - #{$slots_count}"
+      puts "Vehicles count - #{$vehicles_count}"
+      puts "Empty slots count - #{$empty_slots_count}\n"
     end
 
     def display_slots
@@ -44,21 +48,22 @@ module Methods
     end
 
     def update_count_success_view
-      @vehicles_count += 1
-      @empty_slots_count -= 1
-      puts '----------------vehicle added successfully------------------'
+      $vehicles_count += 1
+      $empty_slots_count -= 1
+      puts `----------------vehicle added successfully------------------ \n`
     end
 
     def add_vehicle(vehicle_object)
       (0...@slots.length).each do |slot|
-        next unless @slots[slot].zero?
-
-        @slots[slot] = vehicle_object
-        update_count_success_view
-        return ''
+        if @slots[slot] == 0
+          @slots[slot] = vehicle_object
+          update_count_success_view
+          return ''
+        end
       end
-      parking.addSlot
-      @slots[-1] = vehicle_bject
+      add_slot
+      @slots[-1] = vehicle_object
+      print @slots
       update_count_success_view
     end
 
@@ -73,12 +78,13 @@ module Methods
       (0...@slots.length).each do |i|
         next if slot_id != i
 
-        if @slots[i].zero?
+        if @slots[i] == 0
           puts `The slot you selected is not having vehicle \n`
           return ''
         end
         @slots[i] = 0
-        puts `Vehicle removed! \n #{return ''}`
+        $vehicles_count -= 1
+        puts `Vehicle removed! #{return ''}`
       end
       puts `Entered invalid slot id ! \n`
     end
@@ -91,29 +97,29 @@ module Methods
       puts 'Enter the vehicle lisence number:'
       lisence_number = gets.chomp
       vehicle = Vehicle.new(user_name, vehicle_number, lisence_number)
-      addVehicle(vehicle.vehicleObject)
+      add_vehicle(vehicle.get_vehicle_object)
     end
 
     def display_and_remove_vehicle
       display_vehicles
       slot_id = gets.chomp.to_i
-      removeVehicle(slot_id)
+      remove_vehicle(slot_id)
     end
 
     def activate_selected_option(option)
-      add_slot if option == '1'
-      add_user_vehicle_data if option == '2'
-      display_and_remove_vehicle if option == '3'
-      if option == '4'
+      case option
+      when '1'
+        add_slot
+      when '2'
+        add_user_vehicle_data
+      when '3'
+        display_and_remove_vehicle
+      when '4'
         display_slots
         display_parking_details
       else
         puts 'Invalid option ! Enter the valid option !'
       end
-    end
-
-    def complete_task(option)
-      activate_selected_option option
     end
   end
 
@@ -128,7 +134,7 @@ module Methods
       @departure_time = ''
     end
 
-    def vehicle_object
+    def get_vehicle_object
       {
         id: @id,
         user_name: @user_name,
